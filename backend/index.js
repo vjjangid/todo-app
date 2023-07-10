f5898849e97a8d197bcc5d043175d6650b4ef764
Vijay jangid <vjjangid850@gmail.com>
2023-07-10 22:43:44 +0530

    adding cors using vercel file

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,6 @@ const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 3000   ;
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-}));
-
 app.use(bodyParser.json());
 app.use(cookieParser());
 
