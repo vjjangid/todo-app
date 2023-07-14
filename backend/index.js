2cea60f7b26284327d02f36bc5077997a5f38974
Vijay jangid <vjjangid850@gmail.com>
2023-07-15 00:14:04 +0530

    new klfd

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 3000   ;
 
 const corsOptions = {
-    origin: [ 'http://localhost:3000', 'https://thirylpa.com'],
+    origin: [ 'http://localhost:3000', 'https://thirtylpa.com'],
     credentials: true
 };
 app.use(cors(corsOptions));
