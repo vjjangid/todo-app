8c28133e1fc9ec606788f7311a30d28652144533
Vijay jangid <vjjangid850@gmail.com>
2023-07-15 00:58:39 +0530

    API's updated

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -1,8 +1,8 @@
 export const prod = {
-    signup: "https://todo-app-backend-kq0imyupc-vjjangid.vercel.app/signup",
-    login: "https://todo-app-backend-kq0imyupc-vjjangid.vercel.app/login",
-    todos: "https://todo-app-backend-kq0imyupc-vjjangid.vercel.app/todos",
-    logout: "https://todo-app-backend-kq0imyupc-vjjangid.vercel.app/logout"
+    signup: "https://todo-app-backend-o5ewww1y5-vjjangid.vercel.app/signup",
+    login: "https://todo-app-backend-o5ewww1y5-vjjangid.vercel.app/login",
+    todos: "https://todo-app-backend-o5ewww1y5-vjjangid.vercel.app/todos",
+    logout: "https://todo-app-backend-o5ewww1y5-vjjangid.vercel.app/logout"
 }
 
 export const dev = {
